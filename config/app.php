0f6e1e418dcd486b3874eb649d9d222fb3aa51a5
kurosawa3069 <kurosawa3069@gmail.com>
2026-01-22 14:54:02 +0900

    environmentConstruction

--- config/app.php
+++ config/app.php
@@ -65,8 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
-
+    'timezone' => env('APP_TIMEZONE', 'Asia/Tokyo'),
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
@@ -78,7 +77,7 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => env('APP_LOCALE', 'ja'),
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
